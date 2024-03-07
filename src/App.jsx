dcae9ef93d103c4054572d1796516cb31d7a2dda
100JM <bjm_9310@naver.com>
2024-03-08 01:47:11 +0900

    불필요한 상태 방지 & 간소화

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,49 @@ import { useState } from "react";
 
 import Player from "./components/Player";
 import GameBoard from "./components/Gameboard";
+import Log from "./components/Log";
+
+function controlActivePlayer(gameTurns) {
+  let nowPlayer = 'X';
+
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    nowPlayer = 'O';
+  }
+
+  return nowPlayer;
+}
 
 function App() {
-  const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [activePlayer, setActivePlayer] = useState('X');
 
-  function handleActivePlayer() {
-    setActivePlayer((prevActivePlayer) => prevActivePlayer === 'X' ? 'O' : 'X');
+  const activePlayer = controlActivePlayer(gameTurns)
+
+  function handleActivePlayer(rowIndex, colIndex) {
+    // setActivePlayer((prevActivePlayer) => prevActivePlayer === 'X' ? 'O' : 'X');
+
+    setGameTurns((prevTurns) => {
+      const currentPlayer = controlActivePlayer(prevTurns);
+      
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns
+      ];
+
+      return updatedTurns;
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player name="player 2" symbol="O"isActive={activePlayer === 'O'}/>
+          <Player name="player 1" symbol="X" isActive={activePlayer === 'X'} />
+          <Player name="player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectActivePlayer={handleActivePlayer} activeSymbol={activePlayer}/>
+        <GameBoard onSelectActivePlayer={handleActivePlayer} turns={gameTurns} />
       </div>
+      <Log turns={gameTurns} />
     </main>
   )
 }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,25 +1,32 @@
-import { useState } from "react";
-
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ]
 
-function GameBoard({onSelectActivePlayer, activeSymbol}) {
-    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+function GameBoard({onSelectActivePlayer, turns}) {
+    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-    function handleSelectSquare(rowIndex, colIndex) {
-        setGameBoard((prveGameBoard) => {
-            const updateBoard = [...prveGameBoard.map((innerArray) => [...innerArray])];
-            updateBoard[rowIndex][colIndex] = activeSymbol;
-            return updateBoard;
-        })
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prveGameBoard) => {
+    //         const updateBoard = [...prveGameBoard.map((innerArray) => [...innerArray])];
+    //         updateBoard[rowIndex][colIndex] = activeSymbol;
+    //         return updateBoard;
+    //     })
         
-        onSelectActivePlayer();
-        // 배열이나 객체를 상태로 사용하고 업데이트 할때는 원본이 아닌 복사본을 사용하여야한다. 
-        // -> 자바스크립트의 객체는 참조형 데이터이므로 원본의 수정이 이루어지는 시점이 리액트가 실행하는 예정된 상태 업데이트보다 이전이다.
-        //    상태 업데이트가 여러개 예정되어 있을때 오류를 일으킬 가능성이 매우 크다.
+    //     onSelectActivePlayer();
+    //     // 배열이나 객체를 상태로 사용하고 업데이트 할때는 원본이 아닌 복사본을 사용하여야한다. 
+    //     // -> 자바스크립트의 객체는 참조형 데이터이므로 원본의 수정이 이루어지는 시점이 리액트가 실행하는 예정된 상태 업데이트보다 이전이다.
+    //     //    상태 업데이트가 여러개 예정되어 있을때 오류를 일으킬 가능성이 매우 크다.
+    // }
+
+    let gameBoard = initialGameBoard;
+
+    for(const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
     }
 
     return (
@@ -27,7 +34,9 @@ function GameBoard({onSelectActivePlayer, activeSymbol}) {
             {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                        <button onClick={() => onSelectActivePlayer(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                            {playerSymbol}
+                        </button>
                     </li>)}
                 </ol>
             </li>)}
